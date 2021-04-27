9ad72f741faefe78dc9d9a303040f693134b02a7
sebgaw12 <sebastian.gawrys11@gmail.com>
2021-04-27 13:46:15 +0200

    AdminServlet

---
+++ src/main/java/pl/coderslab/web/AdminServlet.java
@@ -0,0 +1,24 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("")
+public class AdminServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        AdminDao adminDao = new AdminDao();
+        List<Admin> admins = adminDao.findAll();
+        System.out.println(admins);
+
+        getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+    }
+}
