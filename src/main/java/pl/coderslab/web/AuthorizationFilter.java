25df1e07099d61cd4b52b5d0fb7a60f8b0d0c8ce
MichalMarchel84 <michal.marchel84@gmail.com>
2021-04-29 13:11:23 +0200

    Readded AuthorizationFilter

---
+++ src/main/java/pl/coderslab/web/AuthorizationFilter.java
@@ -0,0 +1,35 @@
+package pl.coderslab.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/app/*")
+public class AuthorizationFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        HttpSession session = ((HttpServletRequest)request).getSession();
+        Object adminId = session.getAttribute("adminId");
+
+        if (adminId==null) {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.sendRedirect("/login");
+        } else {
+            request.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html;charset=utf-8");
+            request.setAttribute("adminId", session.getAttribute("adminId"));
+            request.setAttribute("adminName", session.getAttribute("adminName"));
+            chain.doFilter(request, response);
+        }
+    }
+}
