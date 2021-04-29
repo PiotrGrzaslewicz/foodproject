25e4ddfc796bc687bb3383959b9a53c1e21c0e36
MichalMarchel84 <michal.marchel84@gmail.com>
2021-04-29 14:03:40 +0200

    Fixed bug with character encoding on registration

--- src/main/java/pl/coderslab/web/RegistrationServlet.java
+++ src/main/java/pl/coderslab/web/RegistrationServlet.java
@@ -25,6 +25,7 @@ public class RegistrationServlet extends HttpServlet {
 
         Admin admin = new Admin();
         AdminDao adminDao = new AdminDao();
+        req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=UTF-8");
 
         String firstName = req.getParameter("name");
