5f8a894ba8ea85db2778f7e101e64cb0a1adfb64
piotrgrzaslewicz <pg111@poczta.onet.pl>
2021-05-03 00:28:31 +0200

    poprawka ;-)

--- src/main/java/pl/coderslab/web/EditAdminDataServlet.java
+++ src/main/java/pl/coderslab/web/EditAdminDataServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "EditUserDataServlet", value = "/app/edit-admin-data")
 public class EditAdminDataServlet extends HttpServlet {
@@ -27,9 +28,17 @@ public class EditAdminDataServlet extends HttpServlet {
         AdminDao adminDao = new AdminDao();
         HttpSession session = request.getSession();
         String email = request.getParameter("email");
-        Admin checkAdmin = adminDao.findByEmail(email);
+        List<Admin> admins = adminDao.findAll();
+        boolean res = false;
 
-        if (checkAdmin.getId() > 0) {
+        for (Admin admin : admins) {
+            if (admin.getEmail().equals(email)) {
+                res = true;
+                break;
+            }
+        }
+
+        if (res) {
 
             int id = ((Integer) session.getAttribute("adminId"));
             Admin admin = adminDao.findById(id);
@@ -38,6 +47,7 @@ public class EditAdminDataServlet extends HttpServlet {
             request.setAttribute("errorMsg", errorMsg);
             request.setAttribute("component", "/app/admin/editadmindata.jsp");
             getServletContext().getRequestDispatcher("//app/frame.jsp").forward(request, response);
+
         } else {
 
         Admin admin = adminDao.findById((Integer) session.getAttribute("adminId"));
